64efe52f8592071c7fefba1fee4982d5c700ad4d
Janey <janey.keig@gmail.com>
2015-08-17 13:34:11 -0700

    gradient

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var parent = document.getElementsByTagName("body")[0];
 
 //parent.appendChild(document.querySelector(".class2"));
 
+var gradient = "linear-gradient(getColorGradient, getColorGradient)";
+parent.style.background = "linear-gradient(blue, grey)";
 
 for (i=0; i<63; i++){
 		var newDiv = document.createElement("div"); 
@@ -10,12 +12,30 @@ for (i=0; i<63; i++){
 		newDiv.style.width = "11.1%";
 		newDiv.style.float = "left";
 		newDiv.style.paddingBottom = "11.1%";
-		newDiv.style.backgroundColor = "red";
+		newDiv.style.background = "";
 		parent.appendChild(newDiv);
 
  	if (i % 2 === 0){
  		newDiv.style.backgroundColor = "black";
+ 		newDiv.style.opacity= ".085";
+ 		
  	}
 }
+
+
+function getColorGradient() {
+	
+	var letters = '0123456789ABCDEF'.split('');
+    var color = '#FF12';
+    for (var i = 0; i < 2; i++ ) {
+        color += letters[Math.floor(Math.random() * 14)];
+    }
+  	return color;
+}
+
+
+
+
+
 
 
